7036db85c089973e47c9c0a4f0c52e129d5d2860
szymo <szymon.tomala2@gmail.com>
2024-10-16 20:42:20 +0200

    Create calculator static class, modify templateWorker

---
+++ Rekrutacja/Rekrutacja/Calculator/Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Rekrutacja.Calculator
+{
+    public enum ArithmeticOperation
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division
+    }
+
+    public static class Calculator 
+    {
+        public static double PerformOperation(double firstNumber, double secondNumber, ArithmeticOperation operation)
+        {
+
+            if (Enum.IsDefined(typeof(ArithmeticOperation), (int)operation) is false)
+            {
+                throw new ArgumentException("Not allowed arithmetic operation.");
+            }
+
+            if (operation is ArithmeticOperation.Division && secondNumber == 0)
+            {
+                throw new ArgumentException("It's not allowed to divide by 0.");
+            }
+
+            switch (operation)
+            {
+                case ArithmeticOperation.Addition:
+                    return firstNumber + secondNumber;
+                case ArithmeticOperation.Subtraction:
+                    return firstNumber - secondNumber;
+                case ArithmeticOperation.Multiplication:
+                    return firstNumber * secondNumber;
+                case ArithmeticOperation.Division:
+                    return firstNumber / secondNumber;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
--- Rekrutacja/Rekrutacja/Workers/Template/TemplateWorker.cs
+++ Rekrutacja/Rekrutacja/Workers/Template/TemplateWorker.cs
@@ -2,13 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Soneta.Kadry;
-using Soneta.KadryPlace;
 using Soneta.Types;
 using Rekrutacja.Workers.Template;
-using static Soneta.Business.FieldValue;
+using Rekrutacja.Calculator;
+//using static Soneta.Business.FieldValue;
 
 //Rejetracja Workera - Pierwszy TypeOf określa jakiego typu ma być wyświetlany Worker, Drugi parametr wskazuje na jakim Typie obiektów będzie wyświetlany Worker
 [assembly: Worker(typeof(TemplateWorker), typeof(Pracownicy))]
@@ -19,6 +17,8 @@ namespace Rekrutacja.Workers.Template
         //Aby parametry działały prawidłowo dziedziczymy po klasie ContextBase
         public class TemplateWorkerParametry : ContextBase
         {
+            public TemplateWorkerParametry(Context context) : base(context) { }
+
             [Caption("Data obliczeń")]
             public Date DataObliczen { get; set; }
 
@@ -31,10 +31,6 @@ namespace Rekrutacja.Workers.Template
             [Caption("Operacja")]
             public char OperationSign { get; set; }
 
-            public TemplateWorkerParametry(Context context) : base(context)
-            {
-                this.DataObliczen = Date.Today;
-            }
         }
         //Obiekt Context jest to pudełko które przechowuje Typy danych, aktualnie załadowane w aplikacji
         //Atrybut Context pobiera z "Contextu" obiekty które aktualnie widzimy na ekranie
@@ -57,29 +53,33 @@ namespace Rekrutacja.Workers.Template
             //Pobieranie danych z Contextu
             Pracownik pracownik = null;
 
-            var parameters = (TemplateWorkerParametry)this.Cx[typeof(TemplateWorkerParametry)];
+            var parameters = this.Parametry;
+            var employees = (IEnumerable<Pracownik>)this.Cx[typeof(IEnumerable<Pracownik>)];
 
             ValidateParameters(parameters);
-            var result = PerformOperation(parameters);
 
-            if (this.Cx.Contains(typeof(Pracownik)))
-            {
-                pracownik = (Pracownik)this.Cx[typeof(Pracownik)];
-            }
-
+            var operationResult = Calculator.Calculator
+                .PerformOperation(parameters.A, parameters.B, GetArithmeticOperationFromChar(parameters.OperationSign));
 
             //Modyfikacja danych
             //Aby modyfikować dane musimy mieć otwartą sesję, któa nie jest read only
             using (Session nowaSesja = this.Cx.Login.CreateSession(false, false, "ModyfikacjaPracownika"))
             {
-                
                 //Otwieramy Transaction aby można było edytować obiekt z sesji
                 using (ITransaction trans = nowaSesja.Logout(true))
                 {
                     //Pobieramy obiekt z Nowo utworzonej sesji
-                    //var pracownikZSesja = nowaSesja.Get(pracownik);
+                    var pracownikZSesja = nowaSesja.Get(pracownik);
                     //Features - są to pola rozszerzające obiekty w bazie danych, dzięki czemu nie jestesmy ogarniczeni to kolumn jakie zostały utworzone przez producenta
-                    //pracownikZSesja.Features["DataObliczen"] = this.Parametry.DataObliczen;
+
+                    foreach (var employee in employees)
+                    {
+                        var employeeFromSession = nowaSesja.Get(employee);
+
+                        employeeFromSession.Features["DataObliczen"] = parameters.DataObliczen;
+                        employeeFromSession.Features["Wynik"] = operationResult;
+                    }
+
                     //Zatwierdzamy zmiany wykonane w sesji
                     trans.CommitUI();
                 }
@@ -88,8 +88,30 @@ namespace Rekrutacja.Workers.Template
             }
         }
 
+        private ArithmeticOperation GetArithmeticOperationFromChar(char operationSign)
+        {
+            switch (operationSign)
+            {
+                case '+':
+                    return ArithmeticOperation.Addition;
+                case '-':
+                    return ArithmeticOperation.Subtraction;
+                case '*':
+                    return ArithmeticOperation.Multiplication;
+                case '/':
+                    return ArithmeticOperation.Division;
+                default:
+                    throw new ArgumentException("Invalid operation character.");
+            }
+        }
+
         private void ValidateParameters(TemplateWorkerParametry parameters)
         {
+            if (parameters is null)
+            {
+                throw new ArgumentException("Something went wrong. Try again");
+            }
+
             char[] allowedOperationSigns = { '+', '-', '*', '/' };
 
             if (allowedOperationSigns.Contains(parameters.OperationSign) is false)
@@ -100,23 +122,6 @@ namespace Rekrutacja.Workers.Template
             if (parameters.OperationSign == '/' && parameters.B == 0)
             {
                 throw new ArgumentException("It's not allowed to divide by 0");
-            }
-        }
-
-        private double PerformOperation(TemplateWorkerParametry parameters)
-        {
-            switch (parameters.OperationSign)
-            {
-                case '+':
-                    return parameters.A + parameters.B;
-                case '-':
-                    return parameters.A - parameters.B;
-                case '*':
-                    return parameters.A * parameters.B;
-                case '/':
-                    return parameters.A / parameters.B;
-                default:
-                    return 0;
             }
         }
     }
